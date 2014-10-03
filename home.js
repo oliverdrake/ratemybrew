fc37ad46d812943cebc4f232431116eb24106b96
Oliver Drake <oliver@drake.ch>
2014-10-03 23:41:52 +1300

    more progress

--- home.js
+++ home.js
@@ -5,12 +5,18 @@ Reviews = new Mongo.Collection("reviews");
 
 Router.map(function() {
   this.route('beers', {path: "/", layoutTemplate: 'layout'});
-  this.route('review/:_id', {
+  this.route('score/:_id', {
     template: 'scoresheet',
     layoutTemplate: 'layout',
     data: function() { return Beers.findOne(this.params._id); }
   });
   this.route('reviews', {layoutTemplate: 'layout'});
+  this.route('review/:_id', {
+    template: 'review',
+    layoutTemplate: 'layout',
+    data: function() {
+      return Reviews.findOne(this.params._id);
+    }});
 });
 
 
@@ -27,6 +33,7 @@ if (Meteor.isClient) {
   Template.reviews.reviews = function () {
     return Reviews.find({}, {sort: {submitted: "desc"}});
   }
+
 
 
 }
@@ -57,27 +64,4 @@ if (Meteor.isServer) {
     });
   });
   // /Dirty Hack
-
-
-  // Meteor.methods({
-  //   submitReview: function (rawData, templateData) {
-  //     Mesosphere.reviewForm.validate(rawData, function(errors, formData){
-  //         if(!errors){
-  //           Reviews.insert({
-  //             submitted: new Date(),
-  //             reviewer: "Jim",
-  //             beerId: formData.beerId,
-  //             comments: {
-  //               aroma: formData.aroma,
-  //               flavour: formData.flavour,
-  //             },
-  //           });
-  //         }else{
-  //             _(errors).each( function( value, key ) {
-  //               console.log(key+": "+value.message);
-  //             });
-  //         }
-  //     });
-  //   }
-  // });
 }
